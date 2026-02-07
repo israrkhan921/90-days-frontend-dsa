2715957ac690490c6cac5fabaa657b76e9ba4748
Israr Khan <israrkdp@gmail.com>
2026-02-07 22:53:04 +0430

    i will start work tomorrow on this

--- Day01/Project/src/component/Welcome.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Welcome = () => {
-  return (
-    <h1 className='text-black-900 bg-yellow-800 flex items-center font-bolder justify-center h-screen'>Welcome Israr Khan</h1>
-  )
-}
-
-export default Welcome
