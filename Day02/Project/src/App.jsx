779a0a138436e00cf1c95501c7972440746096bd
Israr Khan <israrkdp@gmail.com>
2026-02-09 00:22:05 +0430

    small project of dsa logic,react UI and props dataflow

--- Day02/Project/src/App.jsx
+++ Day02/Project/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import ArrayDisplay from './component/ArrayDisplay'
 const App = () => {
   return (
-    <>
-    
-    </>
+    <div>
+    <ArrayDisplay numbers={[1, 2, 3, 4, 5]} />
+    </div>
   )
 }
 
 export default App
+
+
+
+
+// about this project:
+// This project is a simple React application that displays an array of numbers along with their sum, minimum, and maximum values. The main component, `App`, renders the `ArrayDisplay` component, which takes an array of numbers as a prop. The `ArrayDisplay` component calculates the sum, minimum, and maximum values using JavaScript array methods and displays them in a styled format using Tailwind CSS classes.
