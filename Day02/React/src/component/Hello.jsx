ce316923184f109946fa597a39fedbc8a5ecb103
Israr Khan <israrkdp@gmail.com>
2026-02-09 21:03:49 +0430

    day03

--- Day02/React/src/component/Hello.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Hello = (props) => {
-  return (
-    <div className='text-red-500 flex justify-center items-center'>Hello {props.name}</div>
-  )
-}
-
-export default Hello
